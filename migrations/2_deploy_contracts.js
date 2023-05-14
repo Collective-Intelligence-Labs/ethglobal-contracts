b04712cf7556a4c76b2b7b134361ba9a363240f6
Alexey Kulik <kulik.aliaksei@gmail.com>
2023-05-14 06:00:49 +0200

    WIP: Update migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,18 +4,38 @@ const Dispatcher = artifacts.require("Dispatcher");
 const EventStore = artifacts.require("EventStore");
 const AggregateRepository = artifacts.require("AggregateRepository");
 
+const AddLiquidityHandler = artifacts.require("AddLiquidityHandler");
+
 module.exports = function(deployer) {
     var output = "";
+
+    
+
     deployer.deploy(Dispatcher).then(() => {
-        try {
-            output += "Dispatcher: " + Dispatcher.address + "\n";
-            var fileName = "./" + deployer.network + ".log";
-            fs.writeFileSync(fileName, output);
-        } catch (err) {
-            console.log(err);
-        } 
+
+        output += "Dispatcher: " + Dispatcher.address + "\n";
+
+        deployer.deploy(EventStore, "0xCf1c39D9Bd5768C853337005Cf6A4499e585d94b").then(() => {
+
+            output += "EventStore: " + EventStore.address + "\n";
+
+            deployer.deploy(AggregateRepository, EventStore.address, Dispatcher.address).then(() => {
+                output += "AggregateRepostory: " + AggregateRepository.address + "\n";
+
+                try {
+                    var fileName = "./" + deployer.network + ".log";
+                    fs.writeFileSync(fileName, output);
+                } catch (err) {
+                    console.log(err);
+                } 
+            });
+        });
+
+        
     });
     
+    
+
     // deployer.deploy(EventStore, "0xCf1c39D9Bd5768C853337005Cf6A4499e585d94b").then(() => {
     //     deployer.deploy(AggregateRepository, EventStore.address);
     // });
